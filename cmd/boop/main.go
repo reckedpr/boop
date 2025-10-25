930bce74611367aa8351d44602f4ae95b45aec26
reckedpr <103007531+reckedpr@users.noreply.github.com>
2025-10-25 13:51:28 +0100

    crude directory listing and file return with traversal links

--- cmd/boop/main.go
+++ cmd/boop/main.go
@@ -2,52 +2,59 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/gin-gonic/gin"
-	"github.com/spf13/pflag"
 )
 
-// need to hit tha hay, head is soo broked right now
+func DisplayDir(c *gin.Context) {
+	reqPath := strings.TrimPrefix(c.Param("filepath"), "/")
+	relPath := path.Clean(reqPath)
+	absRoot, _ := filepath.Abs(".")
 
-func ValidatePath(path string) (IsFile bool, IsDir bool) {
-	fi, err := os.Stat(path)
+	fullPath := filepath.Join(absRoot, relPath)
+
+	fi, err := os.Stat(fullPath)
 	if err != nil {
-		fmt.Println(err)
-		IsFile = true
+		c.String(http.StatusNotFound, "file or direk aint exist ngl")
 	}
 
-	switch mode := fi.Mode(); {
-	case mode.IsDir():
-		IsDir = true
-		return
-	case mode.IsRegular():
-		IsFile = true
-		return
-	}
+	if fi.IsDir() {
+		entries, _ := os.ReadDir(fullPath)
+		html := "<html><body>"
 
-	return
+		if fullPath != absRoot {
+			parentPath := path.Dir(relPath)
+
+			html += fmt.Sprintf(`<a href="/%s">..</a><br>`, parentPath)
+		}
+
+		for _, e := range entries {
+			name := e.Name()
+			link := path.Join(relPath, name)
+			if e.IsDir() {
+				link += "/"
+			}
+			html += fmt.Sprintf(`<a href="/%s">%s</a><br>`, link, name)
+		}
+
+		html += "</body></html>"
+		c.Data(200, "text/html; charset=utf-8", []byte(html))
+	} else {
+		c.File(fullPath)
+	}
 }
 
 func main() {
-	pathFlag := pflag.StringP("path", "p", ".", "file or dirrr to uhhhhh ya")
-	pflag.Parse()
-
 	r := gin.Default()
 
-	isFile, isDir := ValidatePath(*pathFlag)
-
-	if isDir {
-		r.StaticFS("/", gin.Dir(*pathFlag, true))
-	} else if isFile {
-		r.GET("/", func(c *gin.Context) {
-			c.File(*pathFlag)
-		})
-
-	} else {
-		fmt.Println("yeah nah")
-		os.Exit(1)
-	}
+	r.GET("/*filepath", func(c *gin.Context) {
+		DisplayDir(c)
+	})
 
 	r.Run()
 }
--- go.mod
+++ go.mod
@@ -2,10 +2,7 @@ module github.com/reckedpr/boop
 
 go 1.25.1
 
-require (
-	github.com/gin-gonic/gin v1.11.0
-	github.com/spf13/pflag v1.0.10
-)
+require github.com/gin-gonic/gin v1.11.0
 
 require (
 	github.com/bytedance/sonic v1.14.0 // indirect
