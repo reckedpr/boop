79bc7cf9fd5823ce7a4f3347e8277006b7552f05
reckedpr <103007531+reckedpr@users.noreply.github.com>
2025-10-25 19:03:28 +0100

    add flag and stdin support

--- cmd/boop/main.go
+++ cmd/boop/main.go
@@ -3,106 +3,41 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"io"
 	"net/http"
 	"os"
-	"path"
-	"path/filepath"
-	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/reckedpr/boop/internal/web"
+	"github.com/spf13/pflag"
 )
 
-func DisplayDir(c *gin.Context) {
-	reqPath := strings.TrimPrefix(c.Param("filepath"), "/")
-	relPath := path.Clean(reqPath)
-	absRoot, _ := filepath.Abs(".")
-
-	fullPath := filepath.Join(absRoot, relPath)
-
-	fi, err := os.Stat(fullPath)
-	if err != nil {
-		c.String(http.StatusNotFound, "file or direk aint exist ngl")
-	}
-
-	if fi.IsDir() {
-		entries, _ := os.ReadDir(fullPath)
-		var body string
-
-		if fullPath != absRoot {
-			parentPath := path.Dir(relPath)
-
-			body += fmt.Sprintf(`<a href="/%s">..</a><br>`, parentPath)
-		}
-
-		for _, e := range entries {
-			name := e.Name()
-			link := path.Join(relPath, name)
-			class := ""
-			if e.IsDir() {
-				link += "/"
-				name += "/"
-				class += "dir"
-			}
-			body += fmt.Sprintf(`<a class="%s" href="/%s">%s</a><br>`, class, link, name)
-		}
-
-		c.HTML(http.StatusOK, "dirlist", gin.H{
-			"Path": reqPath,
-			"Body": template.HTML(body),
-		})
-	} else {
-		c.File(fullPath)
-	}
-}
-
-const html string = `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>boop /{{.Path}}</title>
-    <style>
-        body{
-            font-family: monospace;
-			background-color: #303446;
-			color: #c6d0f5;
-			padding: 8px 8px;
-        }
-        .list{
-            display: flex;
-            flex-direction: column;
-			font-size: 1.4rem;
-        }
-		a{
-            color: #c6d0f5;
-			text-decoration: none;
-        }
-
-		.dir { color: #a6d189; }
-		.dir :visited{ color: #a6d189; }
-    </style>
-</head>
-<body>
-	<h1>/{{.Path}}</h1>
-	
-    <div class="list">
-		{{.Body}}
-    </div>
-</body>
-</html>
-`
+// do not read the strings, just dont read the strin pls oki thankyu
 
 func main() {
+	pathFlag := pflag.StringP("path", "p", ".", "file or dirrr to uhhhhh ya")
+	pflag.Parse()
 
-	r := gin.Default()
+	gin.SetMode(gin.ReleaseMode)
+	r := gin.New()
 
-	tpl := template.Must(template.New("dirlist").Parse(html))
+	tpl := template.Must(template.New("dirlist").Parse(web.HtmlTemplate))
 
 	r.SetHTMLTemplate(tpl)
 
-	r.GET("/*filepath", func(c *gin.Context) {
-		DisplayDir(c)
-	})
-
+	fi, _ := os.Stdin.Stat()
+	if fi.Mode()&os.ModeCharDevice == 0 {
+		data, _ := io.ReadAll(os.Stdin)
+		//fmt.Println("raw the stdin cum in me, get me boypregnant ;3 mmhhhffgh")
+		r.GET("/", func(c *gin.Context) {
+			c.String(http.StatusOK, string(data))
+		})
+	} else {
+		//fmt.Println("no stdin input, using root or pathy wathy >_<")
+		r.GET("/*filepath", func(c *gin.Context) {
+			web.DisplayDir(c, *pathFlag)
+		})
+		fmt.Printf("boop serving %s\n", *pathFlag)
+	}
 	r.Run()
 }
---
+++ internal/web/render.go
@@ -0,0 +1,99 @@
+package web
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
+
+	"github.com/gin-gonic/gin"
+)
+
+const HtmlTemplate string = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>boop /{{.Path}}</title>
+    <style>
+        body{
+            font-family: monospace;
+			background-color: #303446;
+			color: #c6d0f5;
+			padding: 8px 8px;
+        }
+        .list{
+            display: flex;
+            flex-direction: column;
+			font-size: 1.4rem;
+        }
+		a{
+            color: #c6d0f5;
+			text-decoration: none;
+        }
+
+		.dir { color: #a6d189; }
+		.dir :visited{ color: #a6d189; }
+    </style>
+</head>
+<body>
+	<h1>/{{.Path}}</h1>
+	
+    <div class="list">
+		{{.Body}}
+    </div>
+</body>
+</html>
+`
+
+func DisplayDir(c *gin.Context, servePath string) {
+	reqPath := strings.TrimPrefix(c.Param("filepath"), "/")
+	relPath := path.Clean(reqPath)
+	absRoot, _ := filepath.Abs(servePath)
+
+	fullPath := filepath.Join(absRoot, relPath)
+
+	fi, err := os.Stat(fullPath)
+	if err != nil {
+		c.String(http.StatusNotFound, "file or direk aint exist ngl")
+		return
+	}
+
+	if fi.IsDir() {
+		entries, err := os.ReadDir(fullPath)
+		if err != nil {
+			c.String(http.StatusInternalServerError, "cant read the dir :(")
+			return
+		}
+
+		var body string
+
+		if fullPath != absRoot {
+			parentPath := path.Dir(relPath)
+
+			body += fmt.Sprintf(`<a href="/%s">..</a><br>`, parentPath)
+		}
+
+		for _, e := range entries {
+			name := e.Name()
+			link := path.Join(relPath, name)
+			class := ""
+			if e.IsDir() {
+				link += "/"
+				name += "/"
+				class += "dir"
+			}
+			body += fmt.Sprintf(`<a class="%s" href="/%s">%s</a><br>`, class, link, name)
+		}
+
+		c.HTML(http.StatusOK, "dirlist", gin.H{
+			"Path": reqPath,
+			"Body": template.HTML(body),
+		})
+	} else {
+		c.File(fullPath)
+	}
+}
