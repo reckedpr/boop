aa11e0f509684469710ef783a1452769165ebb13
reckedpr <103007531+reckedpr@users.noreply.github.com>
2025-10-25 21:23:13 +0100

    use positional args

--- cmd/boop/main.go
+++ cmd/boop/main.go
@@ -12,11 +12,20 @@ import (
 	"github.com/spf13/pflag"
 )
 
-// do not read the strings, just dont read the strin pls oki thankyu
+// hai
 
 func main() {
-	pathFlag := pflag.StringP("path", "p", ".", "file or dirrr to uhhhhh ya")
+	pathFlag := pflag.StringP("path", "p", "", "file or dirrr to uhhhhh ya")
 	pflag.Parse()
+
+	var servePath string
+	if *pathFlag != "" {
+		servePath = *pathFlag
+	} else if len(pflag.Args()) > 0 {
+		servePath = pflag.Args()[0]
+	} else {
+		servePath = "."
+	}
 
 	gin.SetMode(gin.ReleaseMode)
 	r := gin.New()
@@ -27,17 +36,19 @@ func main() {
 
 	fi, _ := os.Stdin.Stat()
 	if fi.Mode()&os.ModeCharDevice == 0 {
+		fmt.Println("boop serving from stdin")
+
 		data, _ := io.ReadAll(os.Stdin)
-		//fmt.Println("raw the stdin cum in me, get me boypregnant ;3 mmhhhffgh")
+
 		r.GET("/", func(c *gin.Context) {
 			c.String(http.StatusOK, string(data))
 		})
 	} else {
-		//fmt.Println("no stdin input, using root or pathy wathy >_<")
+		fmt.Printf("boop serving %s\n", servePath)
+
 		r.GET("/*filepath", func(c *gin.Context) {
-			web.DisplayDir(c, *pathFlag)
+			web.DisplayDir(c, servePath)
 		})
-		fmt.Printf("boop serving %s\n", *pathFlag)
 	}
 	r.Run()
 }
